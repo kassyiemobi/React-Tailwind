6658b26ce2743b13c8593ae5a4e7d65311c39324
kassyiemobi <omeh.kasie@gmail.com>
2022-05-08 13:33:04 +0100

    Analytics and newsletter  component done

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero"
 import Analytics from "./components/Analytics";
+import Newsletter from "./components/Newsletter";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
-      <Analytics/>
+      <Analytics />
+      <Newsletter/>
     </div>
   );
 }
--- src/components/Analytics.js
+++ src/components/Analytics.js
@@ -5,11 +5,14 @@ function Analytics() {
     return (  
         <div className='w-full bg-white py-16 px-4'>
             <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
-                <img src={laptop} alt="" />
-                <div>
-                    <p>DATA ANALYTICS DASHBOARD</p>
-                    <h1> Manage Data Analytics Centrally</h1>
-                    <p>lorem</p>
+                <img className="w-[500px] mx-auto my-4"src={laptop} alt="" />
+                <div className='flex flex-col justify-center'>
+                    <p className='text-[#00df9a] text-xl font-bold' >DATA ANALYTICS DASHBOARD</p>
+                    <h1 className=' md:text-4xl sm:text-3xl text-2xl font-bold py-2'> Manage Data Analytics Centrally</h1>
+                    <p>
+                        Lorem Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit amet consectetur adipisci[ng] velit, sed quia non numquam [do] eius modi tempora inci[di]dunt, ut labore et dolore magnam aliquam quaerat voluptatem. 
+                    </p>
+                 <button className='bg-black text-[#00df9a]  rounded w-[200px] font-meduim my-6 py-3 mx-auto md:mx-0'>Get Started</button>
 
                 </div>
 
